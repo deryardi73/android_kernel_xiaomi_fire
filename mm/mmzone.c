28ba331c8e098a805283e3dbcdf14d4b33bd595e
deryardi73 <deryardi73@gmail.com>
2026-07-01 00:34:37 +0700

    MGLRU: Backport MGLRU from dynamic kernel 4.19 oneplus sdm845 and commit adapted from xxblebleblexx

--- mm/mmzone.c
+++ mm/mmzone.c
@@ -97,6 +97,8 @@ void lruvec_init(struct lruvec *lruvec)
 		INIT_LIST_HEAD(&lruvec->lists[lru]);
 }
 
+lru_gen_init_lruvec(lruvec);
+
 #if defined(CONFIG_NUMA_BALANCING) && !defined(LAST_CPUPID_NOT_IN_PAGE_FLAGS)
 int page_cpupid_xchg_last(struct page *page, int cpupid)
 {
