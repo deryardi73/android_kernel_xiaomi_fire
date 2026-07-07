99864aab330aeadf410b36fcd63e67537d39c129
deryardi73 <deryardi73@gmail.com>
2026-07-07 12:32:30 +0700

    eem: temporary fix heavy undervolt gpu

--- drivers/misc/mediatek/base/power/eem_v2/mt6768/mtk_eem.c
+++ drivers/misc/mediatek/base/power/eem_v2/mt6768/mtk_eem.c
@@ -203,7 +203,7 @@ unsigned int ptp_data[3] = {0, 0, 0};
 unsigned int gpu_opp0_t_volt[6] = {
 	105000, 105000, 102500, 100000, 97500, 95000
 };
-unsigned int gpu_vb[3] = {95000, 91250, 87500};
+unsigned int gpu_vb[3] = {105000, 105000, 105000};
 unsigned int gpu_vb_flag;
 unsigned int gpu_vb_volt;
 
