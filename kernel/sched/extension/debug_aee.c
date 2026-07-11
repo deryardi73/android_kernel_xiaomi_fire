ffbed3d25d5a8b73150713b5cdd7a6322a549bb3
deryardi73 <deryardi73@gmail.com>
2026-07-11 19:24:52 +0700

    Update debug_aee.c

--- kernel/sched/extension/debug_aee.c
+++ kernel/sched/extension/debug_aee.c
@@ -416,8 +416,7 @@ void print_cfs_rq_at_AEE(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq)
 #ifdef DEBUG
 	SEQ_printf_at_AEE(m, "  .%-30s: %lu\n", "spread0",
 		SPLIT_NS(spread0));
-	SEQ_printf_at_AEE(m, "  .%-30s: %d\n", "nr_spread_over",
-		cfs_rq->nr_spread_over);
+	/* nr_spread_over removed from struct cfs_rq upstream; no longer printed. */
 #endif
 
 	SEQ_printf_at_AEE(m, "  .%-30s: %d\n",
