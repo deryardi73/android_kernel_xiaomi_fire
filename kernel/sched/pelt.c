e4a86bbcb52738271a023cc83c4a2ad61640a2d5
deryardi73 <deryardi73@gmail.com>
2026-08-06 13:16:22 +0700

    sched, pelt: set pelt multiplier to 2

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -411,7 +411,7 @@ int update_irq_load_avg(struct rq *rq, u64 running)
 __read_mostly unsigned int sched_pelt_lshift;
 
 #ifdef CONFIG_SYSCTL
-unsigned int sysctl_sched_pelt_multiplier = 1;
+unsigned int sysctl_sched_pelt_multiplier = 2;
 
 int sched_pelt_multiplier(struct ctl_table *table, int write, void *buffer,
 			  size_t *lenp, loff_t *ppos)
