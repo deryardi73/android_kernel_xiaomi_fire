bb370334240cf91f766d76bac2565e7da084b9f5
Sultan Alsawaf <sultan@kerneltoast.com>
2026-08-19 00:00:56 +0700

    cpumask: Add cpumasks for big and LITTLE CPU clusters
    Add cpu_lp_mask and cpu_perf_mask to represent the CPUs that
    belong to each cluster in a bi-cluster, heterogeneous system.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -100,6 +100,9 @@ extern struct cpumask __cpu_isolated_mask;
 #ifdef CONFIG_MTK_SCHED_EXTENSION
 #define cpu_isolated_mask   ((const struct cpumask *)&__cpu_isolated_mask)
 #endif
+extern const struct cpumask *const cpu_lp_mask;
+extern const struct cpumask *const cpu_perf_mask;
+
 #if NR_CPUS > 1
 #define num_online_cpus()	cpumask_weight(cpu_online_mask)
 #define num_possible_cpus()	cpumask_weight(cpu_possible_mask)
